670d4a1e3cfc370a16e805d4834537b5eea24dfd
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2023-09-10 11:53:55 +0300

    feat: add getters and setters to Account and Client classes

--- src/main/java/edu/vsu/putinpa/model/Account.java
+++ src/main/java/edu/vsu/putinpa/model/Account.java
@@ -1,14 +1,76 @@
-package edu.vsu.putin_p_a.model;
+package edu.vsu.putinpa.model;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 public class Account {
-    private UUID uuid;
+    private final UUID uuid;
     private String name;
-    private LocalDateTime whenOpened;
+    private final LocalDateTime whenOpened;
     private LocalDateTime whenClosed;
-    private Client creator;
-    private List<Client> owners;
+    private final Client creator;
+    private final List<Client> owners;
+
+    {
+        uuid = UUID.randomUUID();
+        whenOpened = LocalDateTime.now();
+        owners = new ArrayList<>();
+    }
+
+    public Account(String name, Client creator) {
+        this.name = name;
+        this.creator = creator;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getWhenOpened() {
+        return whenOpened;
+    }
+
+    public LocalDateTime getWhenClosed() {
+        return whenClosed;
+    }
+
+    public void setWhenClosed(LocalDateTime whenClosed) {
+        this.whenClosed = whenClosed;
+    }
+
+    public Client getCreator() {
+        return creator;
+    }
+
+    public List<Client> getOwners() {
+        return List.copyOf(owners);
+    }
+
+    public void addOwner(Client owner) {
+        owners.add(owner);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return Objects.equals(uuid, account.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid);
+    }
 }
--- src/main/java/edu/vsu/putinpa/model/Client.java
+++ src/main/java/edu/vsu/putinpa/model/Client.java
@@ -1,13 +1,65 @@
-package edu.vsu.putin_p_a.model;
+package edu.vsu.putinpa.model;
 
-import edu.vsu.putin_p_a.model.operations.Operation;
+import edu.vsu.putinpa.model.operations.Operation;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 public class Client {
-    private UUID uuid;
+    private final UUID uuid;
     private String name;
-    private List<Account> accounts;
-    private List<Operation> operationsHistory;
+    private final LocalDateTime whenCreated;
+    private final List<Account> accounts;
+    private final List<Operation> operationsHistory;
+
+    {
+        uuid = UUID.randomUUID();
+        whenCreated = LocalDateTime.now();
+        accounts = new ArrayList<>();
+        operationsHistory = new ArrayList<>();
+    }
+
+    public Client(String name) {
+        this.name = name;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getWhenCreated() {
+        return whenCreated;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public List<Operation> getOperationsHistory() {
+        return operationsHistory;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(getUuid(), client.getUuid());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUuid());
+    }
 }
