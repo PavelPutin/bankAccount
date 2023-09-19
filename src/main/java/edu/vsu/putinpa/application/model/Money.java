8e3af9fb1840050c4496e01403b6d615e34673d3
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2023-09-19 20:37:08 +0300

    feat: add non-negative constrate to money value

--- src/main/java/edu/vsu/putinpa/application/model/Money.java
+++ src/main/java/edu/vsu/putinpa/application/model/Money.java
@@ -3,6 +3,11 @@ package edu.vsu.putinpa.application.model;
 import java.math.BigDecimal;
 
 public record Money(String currency, BigDecimal value) {
+    public Money {
+        if (value.doubleValue() < 0) {
+            throw new IllegalArgumentException("Money value should be non-negative");
+        }
+    }
     public Money add(Money other) {
         if (!currency.equals(other.currency())) {
             throw new IncompatibleCurrencyValue(currency, other.currency());
