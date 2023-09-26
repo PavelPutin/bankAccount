bbd24d229ab422ce4f877e83ebe7267fccf9fcfa
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2023-09-26 08:33:41 +0300

    feat: add methods to operations history service

--- src/main/java/edu/vsu/putinpa/application/service/OperationsHistoryService.java
+++ src/main/java/edu/vsu/putinpa/application/service/OperationsHistoryService.java
@@ -1,5 +1,6 @@
 package edu.vsu.putinpa.application.service;
 
+import edu.vsu.putinpa.application.model.Account;
 import edu.vsu.putinpa.application.model.JournalOperation;
 
 import java.util.Collection;
@@ -7,4 +8,10 @@ import java.util.Collection;
 public interface OperationsHistoryService {
     void add(Operation<?> operation);
     Collection<JournalOperation> getAll();
+    Collection<JournalOperation> getTransfers();
+    Collection<JournalOperation> getTransfers(Account account);
+    Collection<JournalOperation> getIncomingTransfers();
+    Collection<JournalOperation> getIncomingTransfers(Account account);
+    Collection<JournalOperation> getOutgoingTransfers();
+    Collection<JournalOperation> getOutgoingTransfers(Account account);
 }
