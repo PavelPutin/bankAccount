fa5c0cd7fe549060c1a270625cd8bc177b01dfc6
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2023-11-06 16:16:58 +0300

    feat: create the first version of repositories creator

--- src/main/java/edu/vsu/putinpa/infrastructure/orm/RepositoriesCreator.java
+++ src/main/java/edu/vsu/putinpa/infrastructure/orm/RepositoriesCreator.java
@@ -5,6 +5,10 @@ import edu.vsu.putinpa.infrastructure.di.api.AutoInjected;
 import edu.vsu.putinpa.infrastructure.di.api.Component;
 import edu.vsu.putinpa.infrastructure.di.api.InitMethod;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Arrays;
 import java.util.Set;
 
 import static edu.vsu.putinpa.infrastructure.util.reflection.ReflectionUtil.findAllClassesUsingClassLoader;
@@ -25,8 +29,25 @@ public class RepositoriesCreator {
         Set<Class<?>> classes = findAllClassesUsingClassLoader(context.getSourcePackageName());
         for (Class<?> clazz : classes) {
             if (OrmRepository.class.isAssignableFrom(clazz) && clazz.isInterface() && !clazz.equals(OrmRepository.class)) {
-                System.out.println(clazz.getName());
+                OrmRepository<?, ?> repository = (OrmRepository<?, ?>) Proxy.newProxyInstance(OrmRepository.class.getClassLoader(),
+                        new Class[] {OrmRepository.class},
+                        new GetHandler());
+                context.getComponentFactory().registryComponent(repository.getClass().getName(), repository);
             }
+        }
+    }
+
+    static class GetHandler implements InvocationHandler {
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            if (method.getName().contains("find")) {
+                System.out.println("get method");
+            } else if (method.getName().contains("save")) {
+                System.out.println("save method");
+            } else {
+                System.out.println("not get method");
+            }
+            return null;
         }
     }
 }
