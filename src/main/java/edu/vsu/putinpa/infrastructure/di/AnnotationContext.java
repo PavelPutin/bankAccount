de8c1ec64be70cd59fb4161fc05cea5473be543b
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2023-11-06 15:59:34 +0300

    feat: can find all reposritories

--- src/main/java/edu/vsu/putinpa/infrastructure/di/AnnotationContext.java
+++ src/main/java/edu/vsu/putinpa/infrastructure/di/AnnotationContext.java
@@ -1,12 +1,18 @@
 package edu.vsu.putinpa.infrastructure.di;
 
+import edu.vsu.putinpa.infrastructure.di.api.Component;
+
+@Component
 public class AnnotationContext {
     private final ConfigurableListableComponentFactory componentFactory = new ConfigurableListableComponentFactory();
+    private final String sourcePackageName;
 
     public AnnotationContext(String packageName) throws Exception {
+        this.sourcePackageName = packageName;
         AnnotationComponentReader reader = new AnnotationComponentReader(this);
         int foundComponentsAmount = reader.loadComponentDefinitions(packageName);
         int defaultFoundComponentsAmount = reader.loadComponentDefinitions("edu.vsu.putinpa.infrastructure.di.defaultimpl");
+        componentFactory.registryComponent("AnnotationContext", this);
         componentFactory.refreshComponents();
     }
 
@@ -16,5 +22,13 @@ public class AnnotationContext {
 
     public <T> T getComponent(String name, Class<T> type) {
         return type.cast(componentFactory.getComponent(name));
+    }
+
+    public ConfigurableListableComponentFactory getComponentFactory() {
+        return componentFactory;
+    }
+
+    public String getSourcePackageName() {
+        return sourcePackageName;
     }
 }
--- src/main/java/edu/vsu/putinpa/infrastructure/orm/RepositoriesCreator.java
+++ src/main/java/edu/vsu/putinpa/infrastructure/orm/RepositoriesCreator.java
@@ -1,4 +1,32 @@
 package edu.vsu.putinpa.infrastructure.orm;
 
+import edu.vsu.putinpa.infrastructure.di.AnnotationContext;
+import edu.vsu.putinpa.infrastructure.di.api.AutoInjected;
+import edu.vsu.putinpa.infrastructure.di.api.Component;
+import edu.vsu.putinpa.infrastructure.di.api.InitMethod;
+
+import java.util.Set;
+
+import static edu.vsu.putinpa.infrastructure.util.reflection.ReflectionUtil.findAllClassesUsingClassLoader;
+
+@Component
 public class RepositoriesCreator {
+    private OrmConnection connection;
+    private AnnotationContext context;
+
+    @AutoInjected
+    public RepositoriesCreator(OrmConnection connection, AnnotationContext context) {
+        this.connection = connection;
+        this.context = context;
+    }
+
+    @InitMethod
+    public void init() {
+        Set<Class<?>> classes = findAllClassesUsingClassLoader(context.getSourcePackageName());
+        for (Class<?> clazz : classes) {
+            if (OrmRepository.class.isAssignableFrom(clazz) && clazz.isInterface() && !clazz.equals(OrmRepository.class)) {
+                System.out.println(clazz.getName());
+            }
+        }
+    }
 }
