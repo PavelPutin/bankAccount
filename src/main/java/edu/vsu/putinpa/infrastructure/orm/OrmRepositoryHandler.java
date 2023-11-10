8900c6dccb0ede4c109759789ef590dd4ba0874e
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2023-11-10 20:38:17 +0300

    chore: make log for debbug entities fields

--- src/main/java/edu/vsu/putinpa/infrastructure/orm/OrmRepositoryHandler.java
+++ src/main/java/edu/vsu/putinpa/infrastructure/orm/OrmRepositoryHandler.java
@@ -1,11 +1,16 @@
 package edu.vsu.putinpa.infrastructure.orm;
 
 import edu.vsu.putinpa.infrastructure.orm.api.Column;
+import edu.vsu.putinpa.infrastructure.orm.api.JoinColumn;
+import edu.vsu.putinpa.infrastructure.orm.api.ManyToOne;
 import edu.vsu.putinpa.infrastructure.orm.api.Table;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.Optional;
 
 import static edu.vsu.putinpa.infrastructure.util.reflection.ReflectionUtil.getAllDeclaredFieldsFromClassHierarchy;
 import static edu.vsu.putinpa.infrastructure.util.reflection.ReflectionUtil.getAllDeclaredNonStaticFieldsFromClassHierarchy;
@@ -23,8 +28,18 @@ public class OrmRepositoryHandler implements InvocationHandler {
         System.out.println("created repository for " + entityClass.getName() + " for table " + entityClass.getAnnotation(Table.class).value());
         System.out.println("with id class " + entityIdClass.getName());
         System.out.println("=== fields ===");
-        getAllDeclaredNonStaticFieldsFromClassHierarchy(entityClass)
-                .forEach(f -> System.out.println(f.getName() + " " + (f.getAnnotation(Column.class) == null ? "<complex>" : f.getAnnotation(Column.class).value())));
+
+        for (Field f : getAllDeclaredNonStaticFieldsFromClassHierarchy(entityClass)) {
+            String name = f.getName();
+            String column = Optional.ofNullable(f.getAnnotation(Column.class)).map(Column::value).orElse("-");
+            String manyToOne = Optional.ofNullable(f.getAnnotation(ManyToOne.class))
+                    .map(ManyToOne::tableName)
+                    .orElse("-");
+            String joinColumnName = Optional.ofNullable(f.getAnnotation(JoinColumn.class)).map(JoinColumn::name).orElse("-");
+            String joinColumnRef = Optional.ofNullable(f.getAnnotation(JoinColumn.class)).map(JoinColumn::referencedColumnName).orElse("-");
+
+            System.out.printf("%s, %s, %s, %s, %s%n", name, column, manyToOne, joinColumnName, joinColumnRef);
+        }
         System.out.println();
     }
 
