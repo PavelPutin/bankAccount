63a3707188ae65ebd90488b254458ac400440be5
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2023-12-14 11:40:40 +0300

    feat: annotate methods in client repository

--- src/main/java/edu/vsu/putinpa/application/repository/ClientsRepository.java
+++ src/main/java/edu/vsu/putinpa/application/repository/ClientsRepository.java
@@ -3,6 +3,7 @@ package edu.vsu.putinpa.application.repository;
 import edu.vsu.putinpa.application.model.Client;
 import edu.vsu.putinpa.infrastructure.di.api.Component;
 import edu.vsu.putinpa.infrastructure.orm.api.OrmRepository;
+import edu.vsu.putinpa.infrastructure.orm.api.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,8 +11,13 @@ import java.util.UUID;
 
 @Component
 public interface ClientsRepository extends OrmRepository<Client, UUID> {
+    @Query("select * from client where id = ?;")
     Optional<Client> findByUUID(UUID uuid);
+
+    @Query("select * from client where name = ?;")
     Optional<Client> findByName(String name);
+
+    @Query("select * from client;")
     List<Client> findAll();
     Client save(Client client);
 }
